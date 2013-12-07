858a733ac490c28519deca7d565cf3f8d7c7ca01
Carlo Sciolla <skuro@skuro.tk>
2013-12-07 16:13:36 +0100

    Working tooltig, but only for txt pages..?

--- content/getselection.js
+++ content/getselection.js
@@ -4,6 +4,57 @@
  * Respond on queries to provide the selected text on the page
  */
 
+var tooltipTemplate = "<ul>{{#issues}}<li><a href=\"{{link}}\">{{key}}</a></li>{{/issues}}</ul>";
+
+function parseIssues(text){
+    return text.match(/[a-zA-Z]+-[0-9]+/g) || [];
+}
+
+var bubble = document.createElement("div");
+bubble.setAttribute('class', 'GOTOJira_bubble');
+document.body.appendChild(bubble);
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('mouseup', function (e) {
+    var selection = window.getSelection().toString();
+    if (selection.length > 0) {
+        var issues = parseIssues(selection);
+        if(issues.length > 0) {
+            renderBubble(e.clientX, e.clientY, issues);
+        }
+    }
+}, false);
+
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+    //bubble.innerHTML = "";
+    bubble.style.visibility = 'hidden';
+}, false);
+
+function renderBubbleContent(model) {
+    var output = Mustache.render(tooltipTemplate, model);
+    return output;
+}
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, issues) {
+    var issuesLinks = [];
+    issues.forEach(function(issue) {
+        issuesLinks.push({
+            link : "https://backbase.atlassian.net/browse/" + issue,
+            key  : issue
+        });
+    });
+
+    bubble.innerHTML = renderBubbleContent({issues : issuesLinks});
+    bubble.style.top = mouseY + 'px';
+    bubble.style.left = mouseX + 'px';
+    bubble.style.visibility = 'visible';
+
+    debugger;
+}
+
 function noIssuesFoundResponse(){
     return {
         found: false,
