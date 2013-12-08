afa6f06b88332da36a64c00bc2da69f0a5443566
Carlo Sciolla <skuro@skuro.tk>
2013-12-08 09:54:47 +0100

    Removed popup, improved content script, now it seldom works

--- content/getselection.js
+++ content/getselection.js
@@ -4,32 +4,42 @@
  * Respond on queries to provide the selected text on the page
  */
 
-var tooltipTemplate = "<ul>{{#issues}}<li><a href=\"{{link}}\">{{key}}</a></li>{{/issues}}</ul>";
+var tooltipTemplate = "<ul>{{#issues}}<li><a goto-jira=\"link\" href=\"{{link}}\">{{key}}</a></li>{{/issues}}</ul>";
 
 function parseIssues(text){
     return text.match(/[a-zA-Z]+-[0-9]+/g) || [];
 }
 
 var bubble = document.createElement("div");
+bubble.setAttribute('id', 'GOTOJira_bubble');
 bubble.setAttribute('class', 'GOTOJira_bubble');
 document.body.appendChild(bubble);
 
+function isIssueClick(event){
+    var target = event.target;
+    var attrib = target.getAttribute("goto-jira");
+    return attrib != null && "link" == attrib;
+}
+
 // Lets listen to mouseup DOM events.
 document.addEventListener('mouseup', function (e) {
-    var selection = window.getSelection().toString();
-    if (selection.length > 0) {
-        var issues = parseIssues(selection);
-        if(issues.length > 0) {
-            renderBubble(e.clientX, e.clientY, issues);
+    if(!isIssueClick(e)) {
+        var selection = window.getSelection().toString();
+        if (selection.length > 0) {
+            var issues = parseIssues(selection);
+            if(issues.length > 0) {
+                renderBubble(e.clientX, e.clientY, issues);
+            }
         }
     }
 }, false);
 
-
 // Close the bubble when we click on the screen.
 document.addEventListener('mousedown', function (e) {
-    //bubble.innerHTML = "";
-    bubble.style.visibility = 'hidden';
+    if(!isIssueClick(e)) {
+        bubble.innerHTML = "";
+        bubble.style.visibility = 'hidden';
+    }
 }, false);
 
 function renderBubbleContent(model) {
@@ -51,8 +61,6 @@ function renderBubble(mouseX, mouseY, issues) {
     bubble.style.top = mouseY + 'px';
     bubble.style.left = mouseX + 'px';
     bubble.style.visibility = 'visible';
-
-    debugger;
 }
 
 function noIssuesFoundResponse(){
