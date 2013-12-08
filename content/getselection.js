0e73846fb6ff97b505797001325a3aadb72345b1
Carlo Sciolla <skuro@skuro.tk>
2013-12-08 09:55:31 +0100

    Removed unused eventing code

--- content/getselection.js
+++ content/getselection.js
@@ -69,23 +69,3 @@ function noIssuesFoundResponse(){
         text: "no issues found"
     };
 }
-
-function findIssueNumber(){
-    var selection = window.getSelection();
-    if(selection != null && selection != undefined && selection.toString() !== "") {
-        return {
-            found: true,
-            text: selection.toString()
-        };
-    }
-
-    return noIssuesFoundResponse();
-}
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.method == "getSelection") {
-            var selection = findIssueNumber();
-            sendResponse(selection);
-        }
-    });
